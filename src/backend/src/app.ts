bbca9b4c4a5a6ae85fe457511006af4ebcf62574
se1987 <s.tsukuda1987@gmail.com>
2024-09-04 17:06:47 +0900

    Express初期設定完了

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,46 +1,16 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import cors from 'cors';
-// import userRouter from './router/user';
-import logger from './context/logger';
-import morgan from 'morgan'; //アクセスログ
-import transactionsRouter from './router/transactions';
-import categoriesRouter from './router/categories';
-import { PrismaClient } from '@prisma/client';
 
-dotenv.config();
+import userRouter from './router/user';
 
-// const prisma = new PrismaClient();
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
-// ミドルウェアの設定
-app.use(express.json());
+app.use('/user', userRouter);
 
-// morganの設定: winstonにログを送る
-const stream = {
-  write: (message: string) => logger.info(message.trim()),
-};
-// morganミドルウェア設定
-app.use(morgan('combined', { stream }));
-
-// CORSミドルウェアを設定
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3001', // アクセスを許可するオリジン
-    credentials: true, // クレデンシャル (Cookie など) を許可
-  }),
-);
-
-// エンドポイントの設定
-// app.use('/user', userRouter);
-app.use('/api/categories', categoriesRouter);
-app.use('/api/transactions', transactionsRouter);
-
-// サーバーの起動確認
-app.listen(port, async () => {
-  await prisma.$connect();
-  console.log(`Server running on http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-export default app;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
