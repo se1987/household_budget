e846e7c85b057ed9f0902fb06bbae29b9652e371
se1987 <s.tsukuda1987@gmail.com>
2024-09-05 09:48:19 +0900

    cors設定、環境変数の設定

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,11 +1,24 @@
+import dotenv from "dotenv";
 import express from "express";
 import transactionsRouter from "./router/transactions";
 import userRouter from "./router/user";
 import categoriesRouter from "./router/categories";
+import cors from "cors";
+
+// 環境変数の設定
+dotenv.config();
 
 const app = express();
 const port = 4000;
 
+// CORSミドルウェアを設定
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:3001", // アクセスを許可するオリジン
+    credentials: true, // クレデンシャル (Cookie など) を許可
+  })
+);
+// エンドポイントのルーティング設定
 app.use("/user", userRouter);
 app.use("/transactions", transactionsRouter);
 app.use("/categories", categoriesRouter);
@@ -14,8 +27,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// 入出金一覧表示
-
+// サーバーの起動確認
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
